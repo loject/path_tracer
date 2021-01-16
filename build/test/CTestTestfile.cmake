71f973ef8fe7c54f3c1267f3394dae3cfd846c76
loject <29097142+loject@users.noreply.github.com>
2021-01-17 00:23:07 +0700

    .

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/asdf/Desktop/DEV/tmp/test
-# Build directory: C:/Users/asdf/Desktop/DEV/tmp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
